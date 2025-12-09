8d4081abcf1ab42359e9b524c11389ca460ccd24
Kenneth Okoro <kennethokesiena@gmail.com>
2025-12-09 04:39:44 +0100

    created subscription and user models

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { PORT } from './config/env.js';
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
-import subscriptionRouter from './routes/subscription.route.js';
+import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 
 const app = express();
---
+++ models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+        minLength: [2, "Name must be at least 2 characters long"],
+        maxLength: [50, "Name must be at most 50 characters long"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/.+@.+\..+/, "Please fill a valid email address"],
+        minLength: 5,
+        maxLength: 255
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: [6, "Password must be at least 6 characters long"]
+    }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
